66f237f7393e680b4f6297de2f5048af74bf25db
unknown <rifat-sarker>
2023-12-28 15:16:08 +0600

    featured section table inserted to mui data tables library

--- src/FeaturedBlog/FeaturedBlog.jsx
+++ src/FeaturedBlog/FeaturedBlog.jsx
@@ -1,59 +1,58 @@
-import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import MUIDataTable from "mui-datatables";
 import { AuthContext } from "../providers/AuthProvider";
 
 const FeaturedBlog = () => {
-    const {user} = useContext(AuthContext)
-    console.log(user);
+  const { user } = useContext(AuthContext);
   const [blogs, setBlogs] = useState([]);
-  console.log(blogs);
 
   const url = "http://localhost:5000/blog";
+
   useEffect(() => {
     axios.get(url).then((res) => {
       setBlogs(res.data);
     });
   }, []);
 
-  //sort blog by length of long description
+  // Ensure that ldesc is defined before splitting with double spaces
   const sortedBlogs = blogs.sort((a, b) => {
-    const A = a.ldesc.split(" ").length;
-    const B = b.ldesc.split(" ").length;
-    return B - A;
+    const wordsA = (a.ldesc || "").split(" ").length;
+    const wordsB = (b.ldesc || "").split(" ").length;
+    return wordsB - wordsA;
   });
 
-  return (
-    <div className="mt-12">
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th>Serial Number</th>
-            <th>Blog Title</th>
-            <th>Owner</th>
-            <th>Owner Picture</th>
-          </tr>
-        </thead>
-        <tbody className="text-center">
-          {sortedBlogs.slice(0, 10).map((blog, index) => (
-            <tr  key={blog.id}>
-              <td>{index + 1}</td>
-              <td><p>{blog.title}</p></td>
-              <td>{user.email}</td>
-              <td>
-                <img
-                    className="rounded-lg mx-auto my-4"
-                  src={user.photoURL}
-                  alt="Profile"
-                  width="50"
-                  height="50"
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+  // Map the data to the required format
+  const data = sortedBlogs
+    .slice(0, 10)
+    .map((blog, index) => [
+      index + 1,
+      blog.title,
+      user.email,
+      <img
+        className="btn-circle"
+        src={user.photoURL}
+        alt="Profile"
+        width="50"
+        height="50"
+      />,
+    ]);
 
+  // Define columns for the table
+  const columns = [
+    { name: "Serial Number", options: { filter: false } },
+    { name: "Blog Title", options: { filter: false } },
+    { name: "Owner", options: { filter: false } },
+    { name: "Owner Profile Picture", options: { filter: false } },
+  ];
+
+  // Set up the MUIDataTable component
+  const muiTableProps = {
+    columns,
+    data,
+  };
+
+  // Render the MUIDataTable
+  return <MUIDataTable {...muiTableProps} />;
+};
 export default FeaturedBlog;
